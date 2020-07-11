44af1f4c265ebcd7fcf77ea6e6c860b9227721b1
eduardofierro <eduardofierro@gmail.com>
2020-07-11 20:24:56 +0100

    Add most basic skeleton

--- scrapper/sil_url_discovery.py
+++ scrapper/sil_url_discovery.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+
+from scrapper.pdfs_download import MexLegScrapper
+
+
+class URLDiscoverySIL:
+
+    def __init__(self):
+
+        self.url_to_scrape = None
+        self.search_url = 'http://sil.gobernacion.gob.mx/portal/AsuntosLegislativos/busquedaBasica'
+
+        # TODO: This is reall ugly. Need to refactor somehow.
+        self.main_soup = MexLegScrapper.get_page_soup(self.search_url)
+
+    def _get_tipo_asuntos(self):
+        raise NotImplementedError
