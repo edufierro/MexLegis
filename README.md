a4477ede5f8b138d7f9a5c80d592f3ebfaa038a5
eduardofierro <eduardofierro@gmail.com>
2021-01-30 18:36:30 +0000

    WIP: Model discovery

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Install with:
 ```buildoutcfg
 brew install xpdf
 ```
+
+Necessary to url-discovery
+ChromeDriver.
+https://sites.google.com/a/chromium.org/chromedriver/home
+export CHROMEPATH=./path/to/chromedriver
--- scrapper/sil_url_discovery.py
+++ scrapper/sil_url_discovery.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from scrapper.pdfs_download import MexLegScrapper
+import os
+import time
+
+from utils.click_utlis import info, error
+from selenium import webdriver
 
 
 class URLDiscoverySIL:
 
-    def __init__(self):
+    def __init__(self, selenium_driver=None):
 
         self.url_to_scrape = None
+        # This is hard coded.
         self.search_url = 'http://sil.gobernacion.gob.mx/portal/AsuntosLegislativos/busquedaBasica'
+        info('Searching url {}'.format(self.search_url))
 
-        # TODO: This is reall ugly. Need to refactor somehow.
-        self.main_soup = MexLegScrapper.get_page_soup(self.search_url)
+        self.driver_path = os.environ.get('CHROMEPATH', selenium_driver)
+        if not self.driver_path:
+            error('No Selenium Driver Found. You can set it with CHROMEPATH env variable')
 
-    def _get_tipo_asuntos(self):
-        raise NotImplementedError
+    def _get_soup_using_selenium(self):
+        driver = webdriver.Chrome('/Users/eduardofierro/Downloads/chromedriver')
+        driver.get(self.search_url)
+        time.sleep(2) # Sleeping two seconds,
